0eeef6779d98bcd5d831790712ac2f2eeda596c3
va18hav <va18havrk3@gmail.com>
2026-01-02 21:40:57 +0530

    Update popularInterviews.js

--- src/pages/popularInterviews.js
+++ src/pages/popularInterviews.js
@@ -8,7 +8,7 @@ const popularInterviews = [
         role: "Google SDE-II",
         icon: google,
         level: "Mid-Level",
-        participants: "12.5K",
+        participants: "0",
         rating: 4.8,
         duration: "30 min",
         color: "cyan"
@@ -18,7 +18,7 @@ const popularInterviews = [
         role: "Amazon SDE-III",
         icon: amazon,
         level: "Senior",
-        participants: "8.3K",
+        participants: "0",
         rating: 4.9,
         duration: "45 min",
         color: "blue"
@@ -28,7 +28,7 @@ const popularInterviews = [
         role: "Meta Senior Frontend Engineer",
         icon: meta,
         level: "Senior",
-        participants: "15.2K",
+        participants: "0",
         rating: 4.7,
         duration: "25 min",
         color: "purple"
